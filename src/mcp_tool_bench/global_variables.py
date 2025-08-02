ddbdb23cf53b59c6608a95f652ec8a7c29551d4d
mcp-tool-bench <mcptoolbench@126.com>
2025-08-02 19:55:41 +0800

    Update global_variables.py

--- src/mcp_tool_bench/global_variables.py
+++ src/mcp_tool_bench/global_variables.py
@@ -9,28 +9,27 @@ class Settings(BaseSettings):
     ANTHROPIC_API_KEY: Optional[str] = None
     GOOGLE_API_KEY: Optional[str] = None
     MISTRAL_API_KEY: Optional[str] = None
-    
+    KIMI_API_KEY: Optional[str] = None
+
     model_config = SettingsConfigDict(
         env_file=".env",  
         env_file_encoding="utf-8",
         extra="ignore"            
     )
 
-## Global Settings to Store .env keys
 settings = Settings()
-
-# print (f"DEBUG: Settings settings QWEN_API_KEY {settings.QWEN_API_KEY}")
 
 ## Model Name Enum
 MODEL_SELECTION_CLAUDE_OPUS_4 = "claude-opus-4"
-MODEL_SELECTION_CLAUDE_37 =  "claude-3.7"
+MODEL_SELECTION_CLAUDE_37 =  "claude-3-7-sonnet-20250219"
 MODEL_SELECTION_GPT4O =  "gpt-4o"
 MODEL_SELECTION_GEMINI_25_FLASH = "gemini-2.5-flash"
+MODEL_SELECTION_QWEN25_MAX = "qwen2.5-max"
 MODEL_SELECTION_QWEN3_MAX = "qwen3-max"
 MODEL_SELECTION_QWEN3_PLUS = "qwen3-plus"
 MODEL_SELECTION_DEEPSEEK_R1 = "deepseek-r1"
 MODEL_SELECTION_QWEN3_CODER = "qwen3-coder-plus"
-MODEL_SELECTION_KIMI_K2 = "Kimi-K2-Instruct"
+MODEL_SELECTION_KIMI_K2 = "kimi-k2-0711-preview"
 
 ## Constant KEY 
 KEY_MCP_TOOLS_DICT = "mcp_tools_dict"
@@ -41,4 +40,3 @@ KEY_FUNCTION_CALL = "function_call"
 
 
 EVALUATION_TRIAL_PER_TASK = 5
-
